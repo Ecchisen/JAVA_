d74fa0c9202b5e1664efb34702ab00c376d73daa
tmg_utsav <122513109+Ecchisen@users.noreply.github.com>
2025-01-21 12:37:52 +0000

    ClassUsage

--- project_0/Main.java
+++ project_0/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Xmen xmen = new Xmen();
+        xmen.run();
+    }
+}
+
+ class Xmen {
+    public void run() {
+        String Name;
+        int Age;
+        char gender;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        Name = input.nextLine();
+
+        System.out.print("Enter your age: ");
+        Age = input.nextInt();
+
+        System.out.print("Enter your gender (M/F): ");
+        gender = input.next().charAt(0);
+
+        input.close();   
     }
 }
